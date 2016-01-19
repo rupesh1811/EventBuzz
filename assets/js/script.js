63958a896a493f64e55904ea4acff2af33bf66e2
rupesh1811 <rupeshmaurya60@gmail.com>
2016-01-20 00:23:11 +0530

    Updated on 19-01-2016

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,35 @@ $(document).ready(function(){
   $('.bxslider').bxSlider({
   	preloadImages: 'all',
   	pager: false,
-  	captions: true
+  	captions: true,
+  	auto: true
+  	});
+
+   var submitIcon = $('.searchbox-icon'),
+    inputBox = $('.searchbox-input'),
+    searchBox = $('.searchbox'),
+    isOpen = false;
+
+  submitIcon.click(function() {
+    if (!isOpen) {
+      searchBox.addClass('searchbox-open');
+      inputBox.focus();
+      isOpen = true;
+    } else {
+      searchBox.removeClass('searchbox-open');
+      inputBox.focusout();
+      isOpen = false;
+    }
   });
 });
+
+function buttonUp() {
+  var inputVal = $('.searchbox-input').val();
+  inputVal = $.trim(inputVal).length;
+  if (inputVal !== 0) {
+    $('.searchbox-icon').css('display', 'none');
+  } else {
+    $('.searchbox-input').val('');
+    $('.searchbox-icon').css('display', 'block');
+  }
+}
